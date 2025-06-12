0977c352ae852af94e5c80cb4bc32ef36fa46f98
gaurav0330 <gauravjikar070806@gmail.com>
2025-06-12 15:07:04 +0530

    formate change

--- controllers/economicIndicatorController.js
+++ controllers/economicIndicatorController.js
@@ -15,8 +15,30 @@ exports.addEconomicIndicator = async (req, res) => {
 exports.getEconomicIndicators = async (req, res) => {
   try {
     const data = await EconomicIndicator.find();
-    res.status(200).json(data);
+
+    const result = {
+      economic_indicators: {
+        gdp: { '2018': [], '2019': [] },
+        unemployment_rate: { '2018': [], '2019': [] },
+        inflation_rate: { '2018': [], '2019': [] },
+        consumer_confidence_index: { '2018': [], '2019': [] },
+        interest_rate: { '2018': [], '2019': [] },
+        exchange_rate: { '2018': [], '2019': [] },
+        stock_market_index: { '2018': [], '2019': [] },
+      }
+    };
+
+    // Aggregate all data into arrays by year
+    data.forEach(entry => {
+      Object.keys(result.economic_indicators).forEach(key => {
+        result.economic_indicators[key]['2018'].push(...(entry[key]?.['2018'] || []));
+        result.economic_indicators[key]['2019'].push(...(entry[key]?.['2019'] || []));
+      });
+    });
+
+    res.status(200).json(result);
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
 };
+
